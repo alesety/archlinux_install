f278d1865dd78101c074268eb34303d4fab3a514
alesety <alesety535@gmail.com>
2017-04-23 06:58:11 +0900

    Update application.sh

--- application.sh
+++ application.sh
@@ -3,6 +3,6 @@ zsh zsh-completions neovim git \
 xorg-server xorg-server-utils xorg-xinit \
 xmonad xmonad-contrib xmobar dmenu gmrun termite \
 firefox firefox-i18n-ja fcitx-mozc fcitx-configtool \
-noto-fonts noto-fonts-cjk \
+noto-fonts noto-fonts-cjk ttf-ricty \
 mutt \
 libglvnd libx264
