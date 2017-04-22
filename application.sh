0528788be0d294e38a925512b694a0d22bca4809
alesety <alesety535@gmail.com>
2017-04-23 07:25:06 +0900

    Update application.sh

--- application.sh
+++ application.sh
@@ -4,5 +4,5 @@ xorg-server xorg-server-utils xorg-xinit \
 xmonad xmonad-contrib xmobar dmenu gmrun termite \
 firefox firefox-i18n-ja fcitx-mozc fcitx-configtool \
 noto-fonts noto-fonts-cjk ttf-ricty \
-mutt \
+mutt dropbox-cli \
 libglvnd libx264
